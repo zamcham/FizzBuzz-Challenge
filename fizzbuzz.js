3ff9e4d8950e4c903b61e077681becae2073001e
zamcham <cristian@zamcham.com>
2023-03-02 12:07:29 -0500

    Fix integer discrepancie

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,17 @@ module.exports = function () {
 
   const FizzBuzz = [];
 
-  for (i = 0; i<101; i++) {
+  for (i = 1; i<101; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
       FizzBuzz.push('FizzBuzz')
     } 
     else if (i % 3 == 0){
-      FizzBuzz.push('Buzz')
+      FizzBuzz.push('Fizz')
     }
     else if (i % 5 == 0)
     {
-      FizzBuzz.push('Fizz')
+      FizzBuzz.push('Buzz')
     } 
     else { 
       FizzBuzz.push(i);
@@ -20,3 +20,27 @@ module.exports = function () {
   }
   return FizzBuzz;
 }
+
+// function test(){
+//   const FizzBuzz = [];
+
+//   for (i = 1; i<101; i++) {
+
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       FizzBuzz.push('FizzBuzz')
+//     } 
+//     else if (i % 3 == 0){
+//       FizzBuzz.push('Fizz')
+//     }
+//     else if (i % 5 == 0)
+//     {
+//       FizzBuzz.push('Buzz')
+//     } 
+//     else { 
+//       FizzBuzz.push(i);
+//     }
+//   }
+//   return FizzBuzz;
+// }
+
+// console.log(test());
